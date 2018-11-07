959af4dda83843a560f7325cd7d9c522f70b7364
Simon Besenthal <simon.besenthal@uni-ulm.de>
2018-11-07 16:57:25 +0100

    Added documentation (SGCT)

--- src_sgct/core/app_internal/ApplicationNodeInternal.h
+++ src_sgct/core/app_internal/ApplicationNodeInternal.h
@@ -33,6 +33,10 @@ namespace viscom {
     class ApplicationNodeInternal
     {
     public:
+        /**
+         *  Constructor method.
+         *  @param fwInternal the FrameworkInternal object to create the ApplicationNodeInternal with.
+         */
         ApplicationNodeInternal(FrameworkInternal& fwInternal);
         ApplicationNodeInternal(const ApplicationNodeInternal&) = delete;
         ApplicationNodeInternal(ApplicationNodeInternal&&) = delete;
@@ -40,39 +44,114 @@ namespace viscom {
         ApplicationNodeInternal& operator=(ApplicationNodeInternal&&) = delete;
         virtual ~ApplicationNodeInternal();
 
+        /** Called before a window is created. */
         [[deprecated("All initialization should be moved to the constructor in the future.")]]
         virtual void PreWindow();
+        /** Called in the beginning to initialize the implementation and create the window and OpenGL context. */
         virtual void InitImplementation() = 0;
+        /** Called before each synchronization in each frame to prepare for it. */
         virtual void PreSync();
+        /** Called after each synchronization in each frame to update local information based on the sync. */
         virtual void PostSync();
+        /**
+         *  This method is called once each frame to clear any frame buffer.
+         *  @see ApplicationNodeBase::ClearBuffer.
+         */
         virtual void ClearBuffer(FrameBuffer& fbo);
+        /**
+         *  This method is called once each frame to draw on any frame buffer.
+         *  @see ApplicationNodeBase::DrawFrame.
+         */
         virtual void DrawFrame(FrameBuffer& fbo);
+        /**
+         *  This method is called once each frame to render the GUI and 2D elements.
+         *  @see ApplicationNodeBase::Draw2D.
+         */
         virtual void Draw2D(FrameBuffer& fbo);
+        /** Called each frame after everything has been drawn. */
         virtual void PostDraw();
+        /**
+         *  This method is called when exiting the application.
+         *  @see ApplicationNodeBase::CleanUp.
+         */
         [[deprecated("All initialization should be moved to the destructor in the future.")]]
         virtual void CleanUp();
+        /**
+         *  Called when receiving a message from another node.
+         *  @see ApplicationNodeBase::DataTransferCallback.
+         */
         void DataTransfer(void* receivedData, int receivedLength, std::uint16_t packageID, int clientID);
+        /**
+         *  Called when successfully sending a message to another node.
+         *  @see ApplicationNodeBase::DataAcknowledgeCallback.
+         */
         void DataAcknowledge(std::uint16_t packageID, int clientID);
+        /**
+         *  Called when the connection status changes.
+         *  @see ApplicationNodeBase::DataTransferStatusCallback.
+         */
         void DataTransferStatus(bool connected, int clientID);
+        /** Synchronizes the framework by encoding and sending all synchronized data to the other nodes. */
         void EncodeData();
+        /** Synchronizes the framework by receiving and decoding synchronized data from other nodes. */
         void DecodeData();
 
+        /**
+         *  This method is called once each frame to handle keyboard input.
+         *  @see ApplicationNodeBase::KeyboardCallback.
+         */
         virtual void KeyboardCallback(int key, int scancode, int action, int mods);
+        /**
+         *  This method is called once each frame to handle keyboard character input.
+         *  @see ApplicationNodeBase::CharCallback.
+         */
         virtual void CharCallback(unsigned int character, int mods);
+        /**
+         *  This method is called once each frame to handle mouse button input.
+         *  @see ApplicationNodeBase::MouseButtonCallback.
+         */
         virtual void MouseButtonCallback(int button, int action);
+        /**
+         *  This method is called once each frame to handle the cursor position.
+         *  @see ApplicationNodeBase::MousePosCallback.
+         */
         virtual void MousePosCallback(double x, double y);
+        /**
+         *  This method is called once each frame to handle changes in cursor position.
+         *  @see ApplicationNodeBase::MouseScrollCallback.
+         */
         virtual void MouseScrollCallback(double xoffset, double yoffset);
+        /**
+         *  Called for touch screens to add a cursor.
+         *  @see ApplicationNodeBase::AddTuioCursor.
+         */
         virtual void AddTuioCursor(TUIO::TuioCursor *tcur);
+        /**
+         *  Called each frame for touch screens to update a cursor.
+         *  @see ApplicationNodeBase::UpdateTuioCursor.
+         */
         virtual void UpdateTuioCursor(TUIO::TuioCursor *tcur);
+        /**
+         *  Called for touch screens to remove a cursor.
+         *  @see ApplicationNodeBase::RemoveTuioCursor.
+         */
         virtual void RemoveTuioCursor(TUIO::TuioCursor *tcur);
 
+        /** Returns the current application time. */
         double GetCurrentAppTime() const { return syncInfoLocal_.currentTime_; }
+        /** Returns the time elapsed since the last frame. */
         double GetElapsedTime() const { return elapsedTime_; }
 
+        /** Returns the internal framework. */
         FrameworkInternal & GetFramework() { return fwInternal_; }
 
     protected:
+        /**
+         * Sets the application node implementation object.
+         * @param appNodeImpl the new application node implementation.
+         */
         void SetApplicationNode(std::unique_ptr<ApplicationNodeBase> appNodeImpl) { appNodeImpl_ = std::move(appNodeImpl); }
+        /** Returns the application node implementation object. */
         ApplicationNodeBase* GetApplicationNode() { return appNodeImpl_.get(); }
 
         /** Holds the synchronized object (local). */
