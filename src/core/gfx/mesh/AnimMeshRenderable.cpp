1730666cadd23ecf187ebb1c8d3fa45652293860
Simon Besenthal <simon.besenthal@uni-ulm.de>
2019-01-15 16:40:57 +0100

    AnimMeshRenderable

---
+++ src/core/gfx/mesh/AnimMeshRenderable.cpp
@@ -0,0 +1,169 @@
+/**
+ * @file   MeshRenderable.cpp
+ * @author Sebastian Maisch <sebastian.maisch@uni-ulm.de>
+ * @date   2016.12.15
+ *
+ * @brief  Implementation of a mesh renderable class.
+ */
+
+
+#include "SceneMeshNode.h"
+#include "SubMesh.h"
+#include "core/gfx/Material.h"
+#include "core/gfx/Texture.h"
+#include "core/open_gl.h"
+#include <glm/gtc/matrix_inverse.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+
+#include "AnimMeshRenderable.h"
+
+namespace viscom {
+
+    /**
+     * Constructor.
+     * @param renderMesh the Mesh to use for rendering.
+     * @param program the program used for rendering.
+     */
+    AnimMeshRenderable::AnimMeshRenderable(const Mesh* renderMesh, GLuint vBuffer, GPUProgram* program) :
+        mesh_(renderMesh),
+        vbo_(vBuffer),
+        vao_(0),
+        drawProgram_(program)
+    {
+    }
+
+    /**
+     * Destructor.
+     */
+    AnimMeshRenderable::~AnimMeshRenderable()
+    {
+        if (vbo_ != 0) glDeleteBuffers(1, &vbo_);
+        vbo_ = 0;
+        if (vao_ != 0) glDeleteVertexArrays(1, &vao_);
+        vao_ = 0;
+    }
+
+    /**
+     * Move constructor.
+     * @param orig the original object
+     */
+    AnimMeshRenderable::AnimMeshRenderable(AnimMeshRenderable&& orig) noexcept :
+        mesh_(orig.mesh_),
+        vbo_(orig.vbo_),
+        vao_(orig.vao_),
+        drawProgram_(orig.drawProgram_),
+        uniformLocations_(std::move(orig.uniformLocations_))
+    {
+        orig.mesh_ = nullptr;
+        orig.vbo_ = 0;
+        orig.vao_ = 0;
+        orig.drawProgram_ = nullptr;
+    }
+
+    /**
+     * Move assignment operator.
+     * @param orig the original object
+     */
+    AnimMeshRenderable& AnimMeshRenderable::operator=(AnimMeshRenderable&& orig) noexcept
+    {
+        if (this != &orig) {
+            this->~AnimMeshRenderable();
+            mesh_ = orig.mesh_;
+            vbo_ = orig.vbo_;
+            vao_ = orig.vao_;
+            drawProgram_ = orig.drawProgram_;
+            uniformLocations_ = std::move(orig.uniformLocations_);
+            orig.mesh_ = nullptr;
+            orig.vbo_ = 0;
+            orig.vao_ = 0;
+            orig.drawProgram_ = nullptr;
+        }
+        return *this;
+    }
+
+    void AnimMeshRenderable::DrawAnimated(const glm::mat4& modelMatrix, int animID, double animTime, bool overrideBump) const
+    {
+        glm::mat4 bonePoses_[128];
+        glm::mat4 skinned_[128];
+
+        const auto& invBindPoseMatrices = mesh_->GetInverseBindPoseMatrices();
+
+        for (auto i = 0U; i < mesh_->GetNodes().size(); ++i) {
+            std::size_t boneIndex = mesh_->GetNodes()[i]->GetBoneIndex();
+
+            if (boneIndex == -1) {
+                continue;
+            }
+
+            bonePoses_[i] = mesh_->GetAnimation(animID)->ComputePoseAtTime(boneIndex, animTime);
+        }
+
+        ComputeGlobalBonePose(mesh_->GetRootNode(), bonePoses_);
+
+        for (const auto& node : mesh_->GetNodes()) {
+            if (node->GetBoneIndex() == -1) {
+                continue;
+            }
+            skinned_[node->GetBoneIndex()] =
+                bonePoses_[node->GetNodeIndex()] * invBindPoseMatrices[node->GetBoneIndex()];
+        }
+
+        glUseProgram(drawProgram_->getProgramId());
+        glBindVertexArray(vao_);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh_->GetIndexBuffer());
+        DrawNodeAnimated(modelMatrix, bonePoses_, skinned_, mesh_->GetRootNode(), overrideBump);
+        glBindVertexArray(0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+
+    void AnimMeshRenderable::DrawNodeAnimated(const glm::mat4& modelMatrix, glm::mat4* bonePoses_, glm::mat4* skinned_, const SceneMeshNode* node, bool overrideBump) const
+    {
+        auto localMatrix = node->GetLocalTransform() * modelMatrix;
+        for (std::size_t i = 0; i < node->GetNumberOfSubMeshes(); ++i) {
+            const auto* submesh = &mesh_->GetSubMeshes()[node->GetSubMeshID(i)];
+            DrawSubMeshAnimated(localMatrix, bonePoses_[node->GetSubMeshID(i)], skinned_, submesh, overrideBump);
+        }
+        for (std::size_t i = 0; i < node->GetNumberOfNodes(); ++i) DrawNodeAnimated(localMatrix, bonePoses_, skinned_, node->GetChild(i), overrideBump);
+    }
+
+    void AnimMeshRenderable::DrawSubMeshAnimated(const glm::mat4& modelMatrix, glm::mat4 bonePose_, glm::mat4* skinned_, const SubMesh* subMesh, bool overrideBump) const
+    {
+        if (subMesh->GetNumberOfIndices() == 0) return;
+
+        glUniformMatrix4fv(uniformLocations_[0], 1, GL_FALSE, glm::value_ptr(modelMatrix));
+        glUniformMatrix3fv(uniformLocations_[1], 1, GL_FALSE, glm::value_ptr(glm::inverseTranspose(glm::mat3(modelMatrix))));
+        glUniformMatrix4fv(uniformLocations_[5], skinned_->length(), GL_FALSE, glm::value_ptr(*skinned_));
+        glUniformMatrix4fv(uniformLocations_[6], 1, GL_FALSE, glm::value_ptr(glm::inverse(bonePose_)));
+
+        auto mat = mesh_->GetMaterial(subMesh->GetMaterialIndex());
+        auto matTex = mesh_->GetMaterialTexture(subMesh->GetMaterialIndex());
+        if (matTex->diffuseTex && uniformLocations_.size() > 2) {
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, matTex->diffuseTex->getTextureId());
+            glUniform1i(uniformLocations_[2], 0);
+        }
+        if (matTex->bumpTex && uniformLocations_.size() > 3) {
+            glActiveTexture(GL_TEXTURE1);
+            glBindTexture(GL_TEXTURE_2D, matTex->bumpTex->getTextureId());
+            glUniform1i(uniformLocations_[3], 1);
+            if (!overrideBump) glUniform1f(uniformLocations_[4], mat->bumpMultiplier);
+        }
+
+        glDrawElements(GL_TRIANGLES, subMesh->GetNumberOfIndices(), GL_UNSIGNED_INT,
+            (static_cast<char*> (nullptr)) + (static_cast<std::size_t>(subMesh->GetIndexOffset()) * sizeof(unsigned int)));
+    }
+
+    void AnimMeshRenderable::ComputeGlobalBonePose(const SceneMeshNode* node, glm::mat4* bonePoses_) const
+    {
+        if (node->GetParent()) {
+            bonePoses_[node->GetNodeIndex()] = bonePoses_[node->GetParent()->GetNodeIndex()] * bonePoses_[node->GetNodeIndex()];
+        }
+
+        for (auto i = 0U; i < node->GetNumberOfNodes(); ++i) {
+            ComputeGlobalBonePose(node->GetChild(i), bonePoses_);
+        }
+    }
+}
---
+++ src/core/gfx/mesh/AnimMeshRenderable.h
@@ -0,0 +1,122 @@
+/**
+ * @file   MeshRenderable.h
+ * @author Sebastian Maisch <sebastian.maisch@uni-ulm.de>
+ * @date   2016.12.15
+ *
+ * @brief  Declaration of a mesh renderable class.
+ */
+
+#pragma once
+
+#include "core/main.h"
+#include "Mesh.h"
+#include "core/gfx/GPUProgram.h"
+
+namespace viscom {
+
+    class Mesh;
+
+    /**
+     *  This class renders a mesh with a specific shader. The shader is assumed to have fixed uniform names:
+     *  modelMatrix: the model matrix.
+     *  normalMatrix: the normal matrix.
+     *  diffuseTexture: the diffuse texture.
+     *  bumpTexture: a bump map.
+     *  bumpMultiplier: the bump multiplier.
+     *
+     *  NOT ALL UNIFORM LOCATIONS NEED TO BE USED!
+     *
+     *  The attribute names are determined by the vertex structure.
+     */
+    class AnimMeshRenderable
+    {
+    public:
+        /**
+         *  Creates a new mesh renderable.
+         *  @param renderMesh the mesh to render.
+         *  @param program the GPU program to render the mesh with.
+         */
+        template<class VTX> static std::unique_ptr<AnimMeshRenderable> create(const Mesh* renderMesh, GPUProgram* program);
+
+        virtual ~AnimMeshRenderable();
+        AnimMeshRenderable(const AnimMeshRenderable&) = delete;
+        AnimMeshRenderable& operator=(const AnimMeshRenderable&) = delete;
+        AnimMeshRenderable(AnimMeshRenderable&&) noexcept;
+        AnimMeshRenderable& operator=(AnimMeshRenderable&&) noexcept;
+
+        /**
+        *  Draws the mesh of the mesh renderable with a specified animation clip and time.
+        *  @param modelMatrix the model matrix to draw the mesh with.
+        *  @param animId the indes of the animation.
+        *  @param animTime the time of the animation.
+        *  @param overrideBump flag for bumb map parameters.
+        */
+        void DrawAnimated(const glm::mat4& modelMatrix, int animID, double animTime, bool overrideBump = false) const;
+
+        /**
+         *  Gets the standart uniform locations when a mesh renderable is created.
+         *  @param program the GPU program to bind the uniforms to.
+         */
+        template<class VTX> void NotifyRecompiledShader(const GPUProgram* program);
+
+    protected:
+        /** Constructor method.
+         *  @param renderMesh the mesh to render.
+         *  @param vBuffer the meshs vertex buffer.
+         *  @param program the GPU program to be used with the mesh.
+         */
+        AnimMeshRenderable(const Mesh* renderMesh, GLuint vBuffer, GPUProgram* program);
+
+        /**
+         *  Draws a node and all its child nodes of the mesh.
+         *  @param modelMatrix the model matrix to draw the mesh with.
+         *  @param node the node to draw.
+         *  @param overrideBump flag for bumb map parameters.
+         */
+        void DrawNodeAnimated(const glm::mat4& modelMatrix, glm::mat4* bonePoses_, glm::mat4* skinned_, const SceneMeshNode* node, bool overrideBump = false) const;
+
+    private:
+        /** Holds the mesh to render. */
+        const Mesh* mesh_;
+        /** Holds the vertex buffer. */
+        GLuint vbo_;
+        /** Holds the vertex array object. */
+        GLuint vao_;
+        /** Holds the rendering GPU program for drawing. */
+        GPUProgram* drawProgram_;
+        /** Holds the standard uniform bindings. */
+        std::vector<GLint> uniformLocations_;
+
+        /**
+         *  Draws a sub mesh of the mesh renderable.
+         *  @param modelMatrix the model matrix to draw the sub mesh with.
+         *  @param subMesh the sub mesh to be drawn.
+         *  @param overrideBump flag for bumb map parameters.
+         */
+        void DrawSubMeshAnimated(const glm::mat4& modelMatrix, glm::mat4 bonePose_, glm::mat4* skinned_, const SubMesh* subMesh, bool overrideBump = false) const;
+
+        void ComputeGlobalBonePose(const SceneMeshNode* node, glm::mat4* bonePoses_) const;
+    };
+
+    template <class VTX>
+    std::unique_ptr<AnimMeshRenderable> AnimMeshRenderable::create(const Mesh* renderMesh, GPUProgram* program)
+    {
+        std::unique_ptr<AnimMeshRenderable> result{ new AnimMeshRenderable(renderMesh, VTX::CreateVertexBuffer(renderMesh), program) };
+        result->NotifyRecompiledShader<VTX>(program);
+        return std::move(result);
+    }
+
+    template<class VTX> void AnimMeshRenderable::NotifyRecompiledShader(const GPUProgram* program)
+    {
+        glGenVertexArrays(1, &vao_);
+        glBindVertexArray(vao_);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh_->GetIndexBuffer());
+        VTX::SetVertexAttributes(program);
+        glBindVertexArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+        uniformLocations_ = program->GetUniformLocations({ "modelMatrix", "normalMatrix", "diffuseTexture", "bumpTexture", "bumpMultiplier", "skinningMatrices", "invNodeMatrix" });
+    }
+}
