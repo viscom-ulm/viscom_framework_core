1b190e81687000502b20042196692db681fefc41
Sebastian Maisch <sebastian.maisch@uni-ulm.de>
2020-01-17 14:47:13 +0100

    Added more explicit conversions.

--- src/core/gfx/mesh/MeshRenderable.cpp
+++ src/core/gfx/mesh/MeshRenderable.cpp
@@ -126,7 +126,7 @@ namespace viscom {
             if (!overrideBump) glUniform1f(uniformLocations_[4], mat->bumpMultiplier);
         }
 
-        glDrawElements(GL_TRIANGLES, subMesh->GetNumberOfIndices(), GL_UNSIGNED_INT,
+        glDrawElements(GL_TRIANGLES, static_cast<GLsizei>(subMesh->GetNumberOfIndices()), GL_UNSIGNED_INT,
             reinterpret_cast<char*>(static_cast<std::size_t>(subMesh->GetIndexOffset()) * sizeof(unsigned int)));
     }
 }
